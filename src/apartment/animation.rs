56f6958d528607eb47bc91938b7319483ed40d61
nightlyside <nightlyside@gmail.com>
2021-08-28 00:00:11 +0200

    Adding walking sound

--- src/apartment/animation.rs
+++ src/apartment/animation.rs
@@ -1,6 +1,9 @@
 use bevy::prelude::*;
+use rand::Rng;
 use serde::Deserialize;
 use std::collections::HashMap;
+
+use super::PlayerComponent;
 
 /// Tag for basic (1 row) animation
 pub struct BasicAnimationComponent;
@@ -64,8 +67,50 @@ pub enum CharacterAnimationType {
     RightMove,
 }
 
+impl CharacterAnimationType {
+    fn is_idle(&self) -> bool {
+        matches!(self, CharacterAnimationType::ForwardIdle | CharacterAnimationType::BackwardIdle |
+            CharacterAnimationType::LeftIdle | CharacterAnimationType::RightIdle)
+    }
+}
+
 /// Used for tracking animations of a character entity
 pub struct CharacterAnimationComponent {
     pub timer: Timer,
     pub animation_type: CharacterAnimationType,
 }
+
+pub struct WalkingSound {
+    pub timer: Timer,
+    pub first_time: bool,
+}
+
+pub fn player_walking_sound_system(
+    mut ws_res: ResMut<WalkingSound>,
+    mut player_query: Query<
+        &mut CharacterAnimationComponent,
+        With<PlayerComponent>,
+    >,
+    audio: Res<Audio>,
+    asset_server: Res<AssetServer>,
+    time: Res<Time>,
+) {
+    for char_animation in player_query.iter_mut() {
+        if !char_animation.animation_type.is_idle() {
+            ws_res.timer.tick(time.delta());
+            if ws_res.timer.finished() || ws_res.first_time {
+                if ws_res.first_time {
+                    ws_res.first_time = false;
+                    ws_res.timer.reset();
+                }
+
+                let index = rand::thread_rng().gen_range(1..8);
+                audio.play(asset_server.load(format!("audio/footstep/footstep-{}.mp3", index).as_str()));
+            }
+        } else {
+            ws_res.timer.reset();
+            ws_res.first_time = true;
+        }
+    }
+}
+
